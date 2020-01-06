dbf2265b3ec58d5a9498706c294c581dbc883034
shivaluma <shivaluma@gmail.com>
2020-01-06 14:22:51 +0700

    update room check

--- Nhom05Booking/app.js
+++ Nhom05Booking/app.js
@@ -3,52 +3,51 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var expressHandlebars= require('express-handlebars');
-var mongoose=require('mongoose');
-var passport=require('passport');
-var connectFlash=require('connect-flash');
-var expressSession=require('express-session');
+var expressHandlebars = require('express-handlebars');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var connectFlash = require('connect-flash');
+var expressSession = require('express-session');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var handlebars=require('handlebars');
+var handlebars = require('handlebars');
 var app = express();
 var request = require('request');
 //mongoose.connect('mongodb://localhost/booking');
-
-
 
 require('./configs/handlebar');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.engine( 'hbs', expressHandlebars( {
-  extname: 'hbs',
-  defaultView: 'default',
-  layoutsDir: __dirname + '/views/layouts',
-  partialsDir: __dirname + '/views/layouts/partials',
-  defaultLayout: "layout"
-}));
-
+app.engine(
+  'hbs',
+  expressHandlebars({
+    extname: 'hbs',
+    defaultView: 'default',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/layouts/partials',
+    defaultLayout: 'layout'
+  })
+);
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(expressSession({secret: 'max', saveUninitialized: false, resave: false}));
+app.use(expressSession({ secret: 'max', saveUninitialized: true, resave: false }));
 app.use(connectFlash());
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(function(req, res, next)
-{
+app.use(function(req, res, next) {
   //login or logout
   //res.locals.isAuthenticated= req.isAuthenticated();
   res.locals.user = req.session.user;
- // res.locals.listrooms = 'xin chao';
+  // res.locals.listrooms = 'xin chao';
   //session cart
- // res.locals.session = req.session;
+  // res.locals.session = req.session;
   res.locals.listRooms = req.session.listRooms;
   res.locals.room = req.session.room;
   res.locals.reservation = req.session.reservation;
@@ -56,23 +55,21 @@ app.use(function(req, res, next)
   next();
 });
 
-app.get('/logout', function(req, res){
+app.get('/logout', function(req, res) {
   res.locals.user = null;
-  req.session.user =null;
+  req.session.user = null;
   req.logout();
   req.session.destroy();
   res.redirect('/');
-  });
+});
 
 app.use('/', indexRouter);
 app.use('/', usersRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -80,7 +77,6 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  
   // render the error page
   res.status(err.status || 500);
   res.render('error');
--- Nhom05Booking/controllers/indexController.js
+++ Nhom05Booking/controllers/indexController.js
@@ -1,107 +1,143 @@
 var roomController = require('../models/room');
 var request = require('request');
 
-module.exports.home =function(req, res, next) {
-    res.render('homes/home', { title: 'N5-BOOKING' })
-};
-
-module.exports.room=function(req, res, next) {
-    if(!req.session.listRooms){
-        request.get('http://nhom05booking.herokuapp.com/room/list', function (error, response, body) {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        var x = JSON.parse(body);
-        var {listRooms} = JSON.parse(body);
-        req.session.listRooms = listRooms;
-        res.locals.listRooms = req.session.listRooms;
-        console.log(res.locals.listRooms)
-        res.render('rooms/room', { title: 'PHÒNG' });
+module.exports.home = function(req, res, next) {
+  if (!req.session.listRooms) {
+    request.get('http://nhom05booking.herokuapp.com/room/list', function(error, response, body) {
+      console.log('error:', error); // Print the error if one occurred
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      var x = JSON.parse(body);
+      var { listRooms } = JSON.parse(body);
+      req.session.listRooms = listRooms;
+      res.locals.listRooms = req.session.listRooms;
+      console.log(res.locals.listRooms);
+      return res.render('homes/home', { title: 'N5-BOOKING' });
     });
-    }else{
-        res.render('rooms/room', { title: 'PHÒNG' });  
-    }
+  } else {
+    return res.render('homes/home', { title: 'N5-BOOKING' });
+  }
 };
 
-module.exports.about =function(req, res, next) {
-    res.render('abouts/about', { layout: 'layout.hbs', title: 'THÔNG TIN' })
-};
-
-module.exports.contact =function(req, res, next) {
-    res.render('contacts/contact', { layout: 'layout.hbs', title: 'LIÊN HỆ' })
-};
-
-module.exports.roomPage=function(req, res, next) {
-    res.render('rooms/room-page', { layout: 'layout.hbs', title: 'PHÒNG' })
-};
-
-module.exports.bookroom=function(req, res, next) {
-    res.render('rooms/book-room', { layout: 'layout.hbs', title: 'PHÒNG' })
-};
-
-module.exports.searchroompost =function(req, res, next){
-        //data = req.body;
-        var query = "adults=" + req.body.adults + "&childs=" + req.body.childs + "&arrivalDate=" + new Date(req.body.arrivalDate).getTime() + "&departureDate=" +new Date(req.body.departureDate).getTime();
-        request.get('http://nhom05booking.herokuapp.com/room/search?'+query, function (error, response, body) {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        var x = JSON.parse(body);
-        var {listRooms} = JSON.parse(body);
-        req.session.listRooms = listRooms;
-        res.locals.listRooms = req.session.listRooms;
-        res.redirect("/room");
+module.exports.room = function(req, res, next) {
+  if (!req.session.listRooms) {
+    request.get('http://nhom05booking.herokuapp.com/room/list', function(error, response, body) {
+      console.log('error:', error); // Print the error if one occurred
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      var x = JSON.parse(body);
+      var { listRooms } = JSON.parse(body);
+      req.session.listRooms = listRooms;
+      res.locals.listRooms = req.session.listRooms;
+      console.log(res.locals.listRooms);
+      res.render('rooms/room', { title: 'PHÒNG' });
     });
-}
+  } else {
+    res.render('rooms/room', { title: 'PHÒNG' });
+  }
+};
 
-module.exports.getRoomInfo = function(req, res, next){
-    var query = "roomId=" + req.query.roomId;
-    var y= 'http://nhom05booking.herokuapp.com/room/info?'+query;
-    request.get(y, function (error, response, body) {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        var x = JSON.parse(body);
-        var {room} = JSON.parse(body);
-        req.session.room = room;
-        res.locals.room = req.session.room;
-        res.redirect("/room-page");
-});
-}
+module.exports.about = function(req, res, next) {
+  res.render('abouts/about', { layout: 'layout.hbs', title: 'THÔNG TIN' });
+};
 
-module.exports.createReservation = function(req, res, next){
-    data = req.body;
-    let arrivalDate = new Date(req.body.arrivalDate);
-    let departureDate = new Date(req.body.departureDate);
-    
-    var x;
-    var room = req.session.room;
-    var token = req.session.token;
-    var user = req.session.user;
-    if(req.body.isPaid === "0"){
-        checkisPaid = 0;
-    }else{
-        checkisPaid =1;
-    }
+module.exports.contact = function(req, res, next) {
+  res.render('contacts/contact', { layout: 'layout.hbs', title: 'LIÊN HỆ' });
+};
 
-   request.post({url: 'http://nhom05booking.herokuapp.com/room/book', headers: {
-    'Authorization': 'bearer '+ token
-  }, form: { 'roomId': room._id ,'userId':  user.id, 'arrivalDate': arrivalDate.getTime(), 'departureDate' : departureDate.getTime(), 'adults': data.adults, 'childs' : data.childs, 'cost':room.price, isPaid : checkisPaid}}, function (error, response, body) {
+module.exports.roomPage = function(req, res, next) {
+  res.render('rooms/room-page', { layout: 'layout.hbs', title: 'PHÒNG' });
+};
+
+module.exports.bookroom = function(req, res, next) {
+  res.render('rooms/book-room', { layout: 'layout.hbs', title: 'PHÒNG' });
+};
+
+module.exports.searchroompost = function(req, res, next) {
+  //data = req.body;
+  var query =
+    'adults=' +
+    req.body.adults +
+    '&childs=' +
+    req.body.childs +
+    '&arrivalDate=' +
+    new Date(req.body.arrivalDate).getTime() +
+    '&departureDate=' +
+    new Date(req.body.departureDate).getTime();
+  request.get('http://nhom05booking.herokuapp.com/room/search?' + query, function(error, response, body) {
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-     x= JSON.parse(body);   
-     let {errorCount} = JSON.parse(body);
-     if(errorCount != null){
-        let messageErrorCheckout = "";
-        for(var i=0; i<parseInt(errorCount); i++){
-            messageErrorCheckout += x.error.room +". ";
+    var x = JSON.parse(body);
+    var { listRooms } = JSON.parse(body);
+    req.session.listRooms = listRooms;
+    res.locals.listRooms = req.session.listRooms;
+    res.redirect('/room');
+  });
+};
+
+module.exports.getRoomInfo = function(req, res, next) {
+  var query = 'roomId=' + req.query.roomId;
+  var y = 'http://nhom05booking.herokuapp.com/room/info?' + query;
+  request.get(y, function(error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    var x = JSON.parse(body);
+    var { room } = JSON.parse(body);
+    req.session.room = room;
+    res.locals.room = req.session.room;
+    res.redirect('/room-page');
+  });
+};
+
+module.exports.createReservation = function(req, res, next) {
+  data = req.body;
+  let arrivalDate = new Date(req.body.arrivalDate);
+  let departureDate = new Date(req.body.departureDate);
+
+  var x;
+  var room = req.session.room;
+  var token = req.session.token;
+  var user = req.session.user;
+  if (req.body.isPaid === '0') {
+    checkisPaid = 0;
+  } else {
+    checkisPaid = 1;
+  }
+
+  request.post(
+    {
+      url: 'http://nhom05booking.herokuapp.com/room/book',
+      headers: {
+        Authorization: 'bearer ' + token
+      },
+      form: {
+        roomId: room._id,
+        userId: user.id,
+        arrivalDate: arrivalDate.getTime(),
+        departureDate: departureDate.getTime(),
+        adults: data.adults,
+        childs: data.childs,
+        cost: room.price,
+        isPaid: checkisPaid
+      }
+    },
+    function(error, response, body) {
+      console.log('error:', error); // Print the error if one occurred
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      x = JSON.parse(body);
+      let { errorCount } = JSON.parse(body);
+      if (errorCount != null) {
+        let messageErrorCheckout = '';
+        for (var i = 0; i < parseInt(errorCount); i++) {
+          messageErrorCheckout += x.error.room + '. ';
         }
-        req.flash("messagecheckout", messageErrorCheckout);
-        res.redirect('/check-out')
-    }else{
-        
-        req.flash("messagecheckout", "Đã đặt phòng thành công");
+        req.flash('messagecheckout', messageErrorCheckout);
         res.redirect('/check-out');
+      } else {
+        req.flash('messagecheckout', 'Đã đặt phòng thành công');
+        res.redirect('/check-out');
+      }
     }
-    });
-}
-module.exports.checkOut=function(req, res, next) {
-    res.render('rooms/check-out', { layout: 'layout.hbs', title: 'Check-out', messagecheckout: req.flash("messagecheckout")  })
+  );
+};
+module.exports.checkOut = function(req, res, next) {
+  res.render('rooms/check-out', { layout: 'layout.hbs', title: 'Check-out', messagecheckout: req.flash('messagecheckout') });
 };
